aa54599a6f50027ddc2fd9305d33ffce0edfa8ae
Corbyn Altman <47550763+caltman24@users.noreply.github.com>
2021-02-15 18:28:24 -0500

    adding starting project

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.querySelector("body");
-var randomButton = document.querySelector("button");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.querySelector("body");
+const randomButton = document.querySelector("button");
 
 // Applies inputed gradients to the background
-function setGradient() {
+const setGradient = () => {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
     css.textContent = body.style.background + ";";
 }
 
 /* Math.floor() function returns the largest integer less than
    or equal to a given number*/
-function generateRandomColor() {
-    var randomColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-    return randomColor;
-}
+const generateRandomColor = () => '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 
 // Applies randomly generated gradients
-function setRandomGradient() {
-    var randomGradient1 = generateRandomColor();
-    var randomGradient2 = generateRandomColor();
-    console.log(randomGradient1 + "   " + randomGradient2);
+const setRandomGradient = () => {
+    const randomGradient1 = generateRandomColor();
+    const randomGradient2 = generateRandomColor();
     body.style.background = "linear-gradient(to right, " + randomGradient1 + ", " + randomGradient2 + ")";
     css.textContent = body.style.background + ";";
     color1.value = randomGradient1;
